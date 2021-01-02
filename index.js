036ce8df9ffaa3e04334501bbbd928408f5cad11
jaywcjlove <398188662@qq.com>
2021-01-02 22:43:54 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 window.require = function (module) {
-  if (!/(uiw|UIW)/.test(module)) {
+  if (module === 'uiw') {
+    if (window.UIW || window.uiw) {
+      return window.UIW || window.uiw;
+    }
     throw new Error('this is a fake require only use for import fusion next');
   }
-  return window.UIW || window.uiw;
 }
