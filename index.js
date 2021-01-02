6c614fbdac86edf7e8e131d782fb1b52bf3cf8bb
小弟调调™ <kennyiseeyou@gmail.com>
2021-01-02 20:27:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 window.require = function (module) {
-  if (module !== 'uiw' && module !== 'UIW') {
+  if (!/(uiw|UIW)/.test(module)) {
     throw new Error('this is a fake require only use for import fusion next');
   }
   return window.UIW || window.uiw;
